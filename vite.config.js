cdcf360865b4bcc2ae1cb10268ca767844113e2d
Gourav004 <gouravthakurpp@gmail.com>
2025-11-13 12:52:30 +0530

    fix: vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),],
+  plugins: [tailwindcss()],
+  preview: {
+    allowedHosts: [
+      'netlify-edl4.onrender.com', // ✅ allow your deployed host
+      'localhost',                 // ✅ optional, for local testing
+    ],
+  },
 })
